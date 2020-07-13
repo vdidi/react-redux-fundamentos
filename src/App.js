63cfbe262442924716f7434011f3fb90c643637d
vdidi <vitor_hugo_di@hotmail.com>
2020-07-13 09:00:22 -0300

    adiciona esquema de cores aos cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <h1>Exercício React-Redux</h1>
+      <div className="linha">
+        <Card title="Card 1" red>X</Card>
+        <Card title="Card 2" green>X</Card>
+      </div>
+      <div className="linha">
+        <Card title="Card 3" blue>Y</Card>
+        <Card title="Card 4" purple>Y</Card>
+      </div>
     </div>
   );
 }
