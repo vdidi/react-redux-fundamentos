32bfaa684031753cab831c1981812ea8ea675e3a
vdidi <vitor_hugo_di@hotmail.com>
2020-07-14 08:47:38 -0300

    passa valores pros components filhos

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+  const { min,  max } = props
   return (
     <Card title="Média dos Números" green>
       <div>
         <span>
-          <span>Mínimo</span>
-          <strong>5</strong>
+          <span>Mínimo: </span>
+          <strong>{(min + max) / 2}</strong>
         </span>
       </div>
     </Card>
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+  const { min,  max } = props
   return (
     <Card title="Soma dos Números" blue>
       <div>
         <span>
-          <span>Resultado:</span>
-          <strong>5</strong>
+          <span>Resultado: </span>
+          <strong>{max + min}</strong>
         </span>
       </div>
     </Card>
