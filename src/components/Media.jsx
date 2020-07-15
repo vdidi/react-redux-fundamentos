77bbaaa0ba6d146ffd2bec98ff9d195a49addfcf
vdidi <vitor_hugo_di@hotmail.com>
2020-07-15 08:44:11 -0300

    connecta os dados no redux na aplicação

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,16 +1,27 @@
-import './Intervalo.css'
 import React from 'react'
+import { connect } from 'react-redux'
 import Card from './Card'
 
-export default props => {
+const Media = props => {
+  const { min, max } = props
+
   return (
     <Card title="Média dos Números" green>
       <div>
         <span>
           <span>Mínimo: </span>
-          <strong>10</strong>
+          <strong>{(max + min) / 2}</strong>
         </span>
       </div>
     </Card>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Media)
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -1,16 +1,26 @@
-import './Intervalo.css'
 import React from 'react'
+import { connect } from 'react-redux'
 import Card from './Card'
 
-export default props => {
+const Soma = props => {
+  const { min, max } = props
   return (
     <Card title="Soma dos Números" blue>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>30</strong>
+          <strong>{min + max}</strong>
         </span>
       </div>
     </Card>
   )
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Soma)
