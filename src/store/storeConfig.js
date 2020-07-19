4c377cb653fa3001a6920127299ea52a622ffc57
vdidi <vitor_hugo_di@hotmail.com>
2020-07-19 11:17:37 -0300

    refatora reducer

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -1,34 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 
+import numerosReducer from './reducers/numeros'
+
 const reducers = combineReducers({
-  numeros: function(state, action) {
-
-    switch(action.type) {
-      case 'NUM_MIN_ALTERADO':
-        return {
-          ...state,
-          min: action.payload
-        }
-      case 'NUM_MAX_ALTERADO':
-        return {
-          ...state,
-          max: action.payload
-        }  
-
-      default:
-        return {
-          min: 7,
-          max: 30
-        }
-    }
-  },
-  nomes: function(state, action) {
-    return [
-      'Ana',
-      'Bia',
-      'Carlos'
-    ]
-  }
+  numeros: numerosReducer,
 })
 
 function storeConfig() {
